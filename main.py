201a38e76d8968f12887adf0ebd8c50ea66c7f34
JoseCarlitosGordo <jose.gordo0806@gmail.com>
2026-03-17 20:55:33 +1030

    familiarising myself with the different physical parts of a chip-8

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import pygame
-
+from constants import COUNTER, WIDTH, HEIGHT
 ram = bytearray(4096)
+
+pc = None
+
+delay_timer = max(max(0, COUNTER), 255) #holder for 8-bit timer
+sound_timer = max(max(0, COUNTER), 255)
+current_instruction = None
+registry = [0]*16
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+while True:
+    pass
